3a3c6c4f7b9c25ff0e26fd2ea221a67f85073b5d
pengju114 <pengju114@163.com>
2014-08-04 13:48:05 +0800

    ASIHTTPRequest .a静态库项目
    新加

---
+++ ASIHTTPRequestTests/ASIHTTPRequestTests.m
@@ -0,0 +1,34 @@
+//
+//  ASIHTTPRequestTests.m
+//  ASIHTTPRequestTests
+//
+//  Created by 陆振文 on 14-8-4.
+//  Copyright (c) 2014年 pj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASIHTTPRequestTests : XCTestCase
+
+@end
+
+@implementation ASIHTTPRequestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
